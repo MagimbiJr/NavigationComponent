e4bd31f76b8fb12b171c4d6e327fea76c570c397
tana <charleatanasi@gmail.com>
2020-10-11 21:46:17 +0300

    Nav Graph and Fragments

---
+++ app/src/main/java/com/tana/navigation/MainFragment.java
@@ -0,0 +1,29 @@
+package com.tana.navigation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class MainFragment extends Fragment {
+    View mView;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_main, container, false);
+        return mView;
+    }
+}
